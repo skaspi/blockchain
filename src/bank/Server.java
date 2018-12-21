9c9fc58fc02dd50c29759c20bcb35b2ad6412e58
dlaguoto <dlaguoto@redhat.com>
2018-12-22 00:56:21 +0200

    adding total order on seq. nodes
    * Protect with mutex the nodes creation
    * Make ID instance field instead of class field
    * Move ROOT creation to constructor

--- src/bank/Server.java
+++ src/bank/Server.java
@@ -1,35 +1,61 @@
 package bank;
-import org.apache.zookeeper.*;
-import java.io.IOException;
 
-public class Server implements Watcher{
-    static ZooKeeper zk = null;
-    static String root = "/ROOT";
-    static int ID;
+import javafx.util.Pair;
+import org.apache.zookeeper.*;
+
+import java.io.IOException;
+import java.util.Collections;
+import java.util.List;
+
+public class Server implements Watcher {
+    private final static String root = "/ROOT";
+    private static ZooKeeper zk = null;
+    private static boolean ordered;
+    private int ID;
+    private int block_id;
+
+    private final Object lock = new Object();
 
     public Server(String zkHost, int id) {
         try {
             zk = new ZooKeeper(zkHost, 3000, this);
             ID = id;
+            block_id = 1;
+            ordered = false;
         } catch (IOException e) {
+            e.printStackTrace();
+        }
+        try {
+            synchronized (lock) {
+                if (zk.exists(root, true) == null) {
+                    zk.create(root, new byte[]{}, ZooDefs.Ids.OPEN_ACL_UNSAFE, CreateMode.PERSISTENT);
+                }
+            }
+        } catch (KeeperException | InterruptedException e) {
             e.printStackTrace();
         }
     }
 
-    public void propose() throws KeeperException, InterruptedException {
-        if (zk.exists(root, true) == null) {
-            zk.create(root, new byte[] {}, ZooDefs.Ids.OPEN_ACL_UNSAFE, CreateMode.PERSISTENT);
-        }
-        zk.create(root + "/", String.valueOf(ID).getBytes(), ZooDefs.Ids.OPEN_ACL_UNSAFE,
+    public void addBlock() throws KeeperException, InterruptedException {
+        ordered = false;
+        Pair<Integer, Integer> pair = new Pair<>(ID, block_id++);
+
+        zk.create(root + "/", pair.toString().getBytes(), ZooDefs.Ids.OPEN_ACL_UNSAFE,
                 CreateMode.PERSISTENT_SEQUENTIAL);
     }
 
+
+    private void totalOrder() throws KeeperException, InterruptedException {
+        synchronized (lock) {
+            List<String> children = zk.getChildren(root, true);
+            Collections.sort(children);
+            ordered = true;
+        }
+    }
+
     public void process(WatchedEvent watchedEvent) {
-        //TODO: Change to more meaningful code.
-        //      For now, it's only for passing compilation.
-        final Event.EventType eventType = watchedEvent.getType();
         try {
-           // electLeader();
+            totalOrder();
         } catch (Exception e) {
             e.printStackTrace();
         }
--- src/blockchain/Main.java
+++ src/blockchain/Main.java
@@ -1,12 +1,14 @@
 package blockchain;
+
 import bank.Server;
 
 public class Main {
     public static void main(String[] args) {
-       //TODO:  Add the main logic of the app
-       Server server = new Server("127.0.0.1:2181", Integer.parseInt(args[0]));
+
+        Server server = new Server("127.0.0.1:2181", Integer.parseInt(args[0]));
         try {
-           server.propose();
+            server.addBlock();
+            //TODO: Make the server stateful
         } catch (Exception e) {
             e.printStackTrace();
         }
