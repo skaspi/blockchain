46a1922ef914ef4d9b187dffa13a81829054ee7f
dlaguoto <dlaguoto@redhat.com>
2018-12-27 15:57:13 +0200

    merged version with pom
    * Inter process communication added
    * Fixed bug on znodes creation
    * Fixed bug on setting watchers on znodes
    * Fixed bug on double updating of client balance
    * Fixed bug on updating batchRunningNumber
    * ISC_buffer : HashMap<Integer,Transaction> --> HashMap<Integer,List<Transaction>>
    * More clean and optimized code

--- src/bank/Server.java
+++ src/bank/Server.java
@@ -1,59 +1,54 @@
 package bank;
-//import org.apache.zookeeper.*;
 
 import org.apache.zookeeper.*;
 
 import java.io.IOException;
-
 import java.net.InetSocketAddress;
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
-public class Server { //implements Watcher{
-    //static ZooKeeper zk = null;
-    static String root = "/ROOT";
-    int ID;
-    static int transactionID = 0;
-    private List<Communicator> communicators;
-    private List<Integer> receivedBatchIDs;
-    private BatchReceiver receiver;
 
-    public Server(int id, List<InetSocketAddress> addresses, int listenerPort) {
-        ID = id;
-        receivedBatchIDs = new ArrayList<>();
-        communicators = new ArrayList<>();
-        for (InetSocketAddress address : addresses)
-        {
-            communicators.add(new Communicator(address.getHostName(), address.getPort()));
-        }
-        receiver = new BatchReceiver(listenerPort, this);
-    }
 public class Server implements Watcher {
-    private final String root = "/ROOT";
+
+    class BatchID {
+        int serverID;
+        int batchNumber;
+
+        BatchID(int serverID, int batchRunningNumber) {
+            this.serverID = serverID;
+            batchNumber = batchRunningNumber;
+        }
+
+        void setBatchNumber(int num) {
+            batchNumber = num;
+        }
+
+        @Override
+        public String toString() {
+            return serverID + "," + batchNumber;
+        }
+    }
+
     private ZooKeeper zk;
-    private ServerData db;
-    private Integer ID;
+    private BatchID batch;
+    private final Integer ID;
+    private BatchReceiver receiver;
+    private int batch_counter = 0;
+    private int transaction_counter = 0;
+    private final String root = "/ROOT";
+    private final String blocks_path = root + "/BLOCKS";
+    private final String servers_path = root + "/SERVERS";
+    private ServerData db = new ServerData();
+    private List<Communicator> communicators = new ArrayList<>();
+    private Set<String> connected_servers = new HashSet<>();
 
-    public void sendMessage(int clientID, int changeBalance) {
-        List<Transaction> transactions = new ArrayList<>();
-        for (int i =0; i< 5; i++) {
-            transactions.add(new Transaction(transactionID++,clientID+i,changeBalance+i));
-        }
-        sendToServers(transactions, transactionID / 5, ID);
-    }
 
-    private void sendToServers(List<Transaction> transactions, int batchID, int senderID) {
-        for (Communicator communicator : communicators) {
-                communicator.sendBatch(transactions, batchID, senderID);
-        }
-    }
-
-/*
-    public Server(String zkHost, int id) {
+    public Server(String zkHost, List<InetSocketAddress> addresses, int id, int listenerPort) {
+        ID = id;
         try {
             zk = new ZooKeeper(zkHost, 3000, this);
-            db = new ServerData();
-            ID = id;
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -61,57 +56,94 @@ public class Server implements Watcher {
             if (zk.exists(root, true) == null) {
                 zk.create(root, new byte[]{}, ZooDefs.Ids.OPEN_ACL_UNSAFE, CreateMode.PERSISTENT);
             }
-            if (zk.exists(root + "/BLOCKS", true) == null) {
-                zk.create(root + "/BLOCKS", new byte[]{}, ZooDefs.Ids.OPEN_ACL_UNSAFE, CreateMode.PERSISTENT);
+            if (zk.exists(blocks_path, true) == null) {
+                zk.create(blocks_path, new byte[]{}, ZooDefs.Ids.OPEN_ACL_UNSAFE, CreateMode.PERSISTENT);
             }
-            if (zk.exists(root + "/SERVERS", true) == null) {
-                zk.create(root + "/SERVERS", new byte[]{}, ZooDefs.Ids.OPEN_ACL_UNSAFE, CreateMode.PERSISTENT);
+            if (zk.exists(servers_path, true) == null) {
+                zk.create(servers_path, new byte[]{}, ZooDefs.Ids.OPEN_ACL_UNSAFE, CreateMode.PERSISTENT);
             }
-            zk.create(root + "/SERVERS/" + ID, new byte[]{}, ZooDefs.Ids.OPEN_ACL_UNSAFE, CreateMode.EPHEMERAL);
-            zk.setData(root + "/SERVERS/" + ID, String.valueOf(ID).getBytes(), -1);
+            zk.create(servers_path + "/" + ID, new byte[]{}, ZooDefs.Ids.OPEN_ACL_UNSAFE, CreateMode.EPHEMERAL);
+            zk.setData(servers_path + "/" + ID, String.valueOf(ID).getBytes(), -1);
+        } catch (KeeperException | InterruptedException e) {
+            e.printStackTrace();
+        }
+        for (InetSocketAddress address : addresses) {
+            communicators.add(new Communicator(address.getHostName(), address.getPort()));
+        }
+        batch = new BatchID(ID, 0);
+        receiver = new BatchReceiver(listenerPort, this);
+    }
 
+    public void addTransaction(int clientID, int changeBalance) throws KeeperException, InterruptedException {
+        db.userCreate(clientID);
+        db.updateBalance(clientID, changeBalance);
+        if (isBatchThreshold()) {
+            sendToServers(db.getTransactions(), batch_counter, ID);
+            addBlock(batch_counter++);
+            db.clearTransaction();
+        }
+        transaction_counter++;
+    }
+
+    private boolean isBatchThreshold() {
+        //TODO: Replace with the logic of adapting batching
+        return (transaction_counter > 1 && (transaction_counter % 3) == 0);
+    }
+
+    private void sendToServers(List<Transaction> transactions, int batchID, int senderID) {
+        for (Communicator communicator : communicators) {
+            communicator.sendBatch(transactions, batchID, senderID);
+        }
+    }
+
+    private void addBlock(int batchRunningNumber) throws KeeperException, InterruptedException {
+        batch.setBatchNumber(batchRunningNumber);
+        zk.create(blocks_path + "/", String.valueOf(batch).getBytes(), ZooDefs.Ids.OPEN_ACL_UNSAFE, CreateMode.PERSISTENT_SEQUENTIAL);
+    }
+
+    private void updateServers(String path){
+        try {
+            Set<String> children = new HashSet<>(zk.getChildren(path, true));
+            if (children.size() > connected_servers.size()) {
+                connected_servers.addAll(children);
+            }
+            else {
+                Set<String> difference = new HashSet<>(connected_servers);
+                difference.removeAll(children);
+                connected_servers = new HashSet<>(children);
+                db.flush(Integer.parseInt(String.valueOf(difference.toArray()[0])));
+                db.print();
+            }
         } catch (KeeperException | InterruptedException e) {
             e.printStackTrace();
         }
     }
 
-    public void addBlock() throws KeeperException, InterruptedException {
-        String ddd = "8,1";
-        zk.create(root + "/BLOCKS/", ddd.getBytes(), ZooDefs.Ids.OPEN_ACL_UNSAFE,
-                CreateMode.PERSISTENT_SEQUENTIAL);
-    }
-
-    private void totalOrder() {
+    private void processRequest(String path) {
         try {
-            System.out.println(zk.getChildren(root + "/BLOCKS", true));
+            //TODO : Remove wasteful syscalls
+            System.out.println(zk.getChildren(root, true));
+            System.out.println(zk.getChildren(servers_path, true));
+            System.out.println(zk.getChildren(blocks_path, true));
         } catch (KeeperException | InterruptedException e) {
             System.err.println("Skipping inconsistency in initial znode creation...");
+        }
+        if (path == null) return;
+
+        if (path.equals(servers_path)) {
+            updateServers(path);
+        }
+        else {
+            //TODO : Blockchain logic here...
         }
     }
 
     public void process(WatchedEvent watchedEvent) {
-        try {
-            totalOrder();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+        processRequest(watchedEvent.getPath());
     }
-*/
-    public void processReceivedBatch(int batchID, int senderID, List<Transaction> transactions) {
 
-        if (!receivedBatchIDs.contains(batchID) && senderID != this.ID)
-        {
-            receivedBatchIDs.add(batchID);
-            sendToServers(transactions, batchID, senderID); // Send originsl senderID in the message
-            System.out.println("Recieved batch number: " + batchID+  " From: " + senderID +  " batch contains following transactions");
-            for (Transaction t: transactions)
-            {
-                System.out.println("Transaction " + t.getTransactionID()+ ": client ID - " + t.getClientID() + " balance change - "+ t.getBalanceChange());
-            }
-        }
-        else {
-            System.out.println("Recieved repeated message number: " + batchID+ "from server: " + senderID);
-
-        }
+    void processReceivedBatch(int batchID, int senderID, List<Transaction> transactions) {
+        db.addBatch(senderID, transactions);
+        db.print();
     }
 }
